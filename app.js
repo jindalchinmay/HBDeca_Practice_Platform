8be1af4e0d225b4eb166f9ab35bd79952ade0c72
blueishfiend692 <aditymakkar000@gmail.com>
2023-07-19 15:50:31 -0400

    removed set

--- app.js
+++ app.js
@@ -149,10 +149,6 @@ app.get('/auth/google/callback',
 app.get("/landing-page", async (req, res) => {
 
   try {
-    res.set(
-      'Cache-Control',
-      'no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0'
-    );
 
 
     if (req.isAuthenticated()) {
